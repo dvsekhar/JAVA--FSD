c2e9a09ff237565501f0f87d53f0736a7009381a
dvsekhar <dvsekhar2001@gmail.com>
2023-12-14 17:08:37 +0530

    Practice Project 1.16

---
+++ URL Rewrite/src/main/java/DashboardServlet.java
@@ -0,0 +1,33 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		
+		 String userId = request.getParameter("userid");
+		 
+		 pw.println("Welcome, " + userId + " to this Dashboard ");
+		
+      pw.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
